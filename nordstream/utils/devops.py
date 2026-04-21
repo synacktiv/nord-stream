b6ca6d520750409d7bfc383518591b15b3187ae1
Hugo Vincent <hugo.vincent@synacktiv.com>
2026-04-21 15:42:15 +0200

    fix: list orgs

--- nordstream/utils/devops.py
+++ nordstream/utils/devops.py
@@ -3,7 +3,10 @@ from nordstream.cicd.devops import DevOps
 from nordstream.utils.log import logger
 
 def listOrgs(token):
-    response = DevOps.getOrgs(token)
-    logger.info("User orgs:")
-    for org in response:
-        logger.raw(f"\t- {org.get('AccountName')}\n", logging.INFO)
+    try:
+        response = DevOps.getOrgs(token)
+        logger.info("User orgs:")
+        for org in response:
+            logger.raw(f"\t- {org.get('AccountName')}\n", logging.INFO)
+    except Exception as e:
+        logger.error(e)
